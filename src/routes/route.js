a3d4faf91a061ad78fb92eaeae598a93c0f80dbe
Dibyakanti Layek <dibyakanti095@gmail.com>
2022-04-09 10:26:28 +0530

    My Api

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,13 +3,94 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/movies', function (req, res) { 
+      let myFavouriteMovies = ["Forest Gump","Notebook","Loveyou Forever","Iron-Man","Iterstellar","World War-z","Avengers-Endgame","Imagine me and you"];
+    res.send( myFavouriteMovies)
 });
+router.get('/movies/:indexNumber', function (req, res) { 
+    const FavWebSeries = ["Dark","Stranger Things","Sweet-Tooth","Black Mirror","Maid","Loki"]
+     const i = req.params.indexNumber 
+     if(i < FavWebSeries.length){
+         res.send(FavWebSeries[i])
+
+     }
+       else{
+           res.send("There's an error")
+       }
+});
+router.get('/flims', function(req, res) {
+    [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+});
+
+router.get('/arry', function(req , res){
+    const n = [1,2,3,4,5,7,8,9]
+    let arr = 
+  res.send(arr) 
+});
+
+
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+    //LOGIC WILL COME HERE
+    for (let i = 0; i < players.length; i++) {
+        console.log(req.body.name === players[i].name);
+        if(req.body.name === players[i].name){
+            res.send({msg: "This player is already exist"})
+            break;
+        } else {
+            players.push(req.body);
+            break;
+        }
+        
+    }
+        res.send(  { data: players , status: true }  )
+ });
+
+
 
 
 

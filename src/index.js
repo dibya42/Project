0fb8bb913a41a33cbc868dd48f18243be3bc4717
Dibyakanti Layek <dibyakanti095@gmail.com>
2022-04-12 09:01:28 +0530

    hi

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ var bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 
 const app = express();
-
+const mongoose = require('mongoose')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://dibya:Zoomhtml1@cluster0.r6e0m.mongodb.net/bittu7876-DB?retryWrites=true&w=majority",{
+useNewUrlParser : true
+})
+.then(  () => console.log("MongoDB is connected"))
+.catch ( err => console.log(err) )
+
 
 app.use('/', route);
 
